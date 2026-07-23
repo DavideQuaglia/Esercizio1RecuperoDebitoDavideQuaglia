6aa3b92c3b1f74e2a142907467dd6620cc6fc8fe
DavideQuaglia <davide.quaglia09@gmail.com>
2026-07-23 15:49:42 +0200

    Commit definitivo - Sito web

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"option strict";
+function invia(){
+    let nome= document.getElementById("nome").value;
+    let cognome= document.getElementById("cognome").value;
+    let ris= document.getElementById("tour").value;
+    alert("signore/a "+ nome + " " + cognome + " ha scelto l'opzione " + ris);
+}
